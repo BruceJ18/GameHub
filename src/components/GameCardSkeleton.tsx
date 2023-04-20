088807af97a1aae7a0708764148631639238d060
Bruce Jacobo <brucejf18@gmail.com>
2023-04-20 11:58:39 -0700

    Add Skeletons.

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,20 @@
+import {
+  Skeleton,
+  Image,
+  SkeletonText,
+  Card,
+  CardBody,
+} from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton height="200px"/>
+        <CardBody>
+          <SkeletonText />
+        </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,27 +23,35 @@ export interface Game {
   }
 
 const useGames = () => {
-     // we need a state variable for storing our game objects
+  // we need a state variable for storing our game objects
   const [games, setGames] = useState<Game[]>([]);
   // we need a state variable for storing our errors if api request fails
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  
+
 
   // send fetch request to the "back-end"
   useEffect(() => {
     const controller = new AbortController();
-
+    
+    setLoading(true);
     apiClient
       .get<FetchedGames>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) =>{ 
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message)
+        setLoading(false);
       });
 
       return () => controller.abort();
   }, []);
 
-  return {games, error};
+  return {games, error, isLoading};
 }
 
 
