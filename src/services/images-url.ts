171bd9470a65e81a7877e7009cd2269c7f47b355
Bruce Jacobo <brucejf18@gmail.com>
2023-04-21 16:10:49 -0700

    Handle games with no Image.

--- src/services/images-url.ts
+++ src/services/images-url.ts
@@ -1,5 +1,8 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
+
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+    if (!url) return noImage;
 
     const index = url.indexOf('media/') + 'media/'.length;
 
