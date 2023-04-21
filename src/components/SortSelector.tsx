64947175f08e5921fbdcb96dd929109abbd25b6c
Bruce Jacobo <brucejf18@gmail.com>
2023-04-21 16:01:03 -0700

    Sort games.

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,14 +1,29 @@
-import { Button, Menu, MenuButton, MenuList } from '@chakra-ui/react'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 
-const SortSelector = () => {
+interface Props {
+    onSelectSortOrder: (sortOrder: string) => void;
+    sortOrder: string;
+}
 
+const SortSelector = ({sortOrder, onSelectSortOrder}: Props) => {
+    const sortOrders = [
+        {value:'', label: 'Relevance'},
+        {value:'-added', label: 'Date Added'},
+        {value:'name', label: 'Name'},
+        {value:'-released', label: 'Release Date'},
+        {value:'-metacritic', label: 'Popularity'},
+        {value:'-rating', label: 'Average Rating'}
+    ]
 
+    const currentSortOrder = sortOrders.find(order => order.value === sortOrder)
   return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown/>}>Order by: Relevance</MenuButton>
+        <MenuButton as={Button} rightIcon={<BsChevronDown/>}>Order by: {currentSortOrder?.label || 'Relevance'}</MenuButton>
         <MenuList>
-
+            {sortOrders.map(order => <MenuItem onClick={() => onSelectSortOrder(order.value)} key={order.value} value={order.value}>
+                {order.label}
+                </MenuItem>)}
         </MenuList>
     </Menu>
     
