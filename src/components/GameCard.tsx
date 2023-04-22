c87029423957d0087c06c34d6e502b9338a928b0
Bruce Jacobo <brucejf18@gmail.com>
2023-04-22 10:22:27 -0700

    Clean up game grid.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -16,12 +16,12 @@ const GameCard = ( {selectedPlatform, game }: Props ) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
         <Image src={getCroppedImageUrl(game.background_image)}/>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            <HStack justifyContent={"space-between"}>
+        <CardBody>   
+            <HStack marginBottom={3} justifyContent={"space-between"}>
                 <Plt_nameToIcon selectedPlatform={selectedPlatform} platforms={game.parent_platforms.map( pObject => pObject.platform )}/>
                 <CriticScore score={game.metacritic}/>
-            </HStack>
+            </HStack >
+            <Heading fontSize='2xl'>{game.name}</Heading>
         </CardBody>
         
     </Card>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,20 +6,22 @@ import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
-
 interface Props {
-  gameQuery: GameQuery
+  gameQuery: GameQuery;
 }
 
-
-const GameGrid = ({gameQuery} : Props) => {
+const GameGrid = ({ gameQuery }: Props) => {
   const { dataArray, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={3}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={6}
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
