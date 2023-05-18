64e19a1cea4943c73ad5ed330ef3973ddf4d91db
Bruce Jacobo <bruceja18@protonmail.com>
2023-05-17 20:02:59 -0700

    Implement game screen shots to game details page.

---
+++ src/components/GameScreenShots.tsx
@@ -0,0 +1,22 @@
+import { Image, SimpleGrid } from '@chakra-ui/react';
+import React from 'react'
+import useScreenshots from '../hooks/useScreenShots';
+
+
+interface Props {
+    gameId: number;
+}
+
+
+const GameScreenShots = ({ gameId } : Props) => {
+    const {data, isLoading, error} = useScreenshots(gameId);
+
+    if (isLoading) return null;
+    if (error) throw error
+
+    return (
+    <SimpleGrid spacing={2} columns={{base: 1, lg: 2}}>{data?.results.map(file => <Image key={file.id} src={file.image}/>)}</SimpleGrid>
+  )
+}
+
+export default GameScreenShots
